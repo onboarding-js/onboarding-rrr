7cd141d9d602f252a03df235aaf49c1692b52d8e
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-01-30 17:04:17 -0300

    add user hooks at home

--- src/pages/Home/indexHooks.js
+++ src/pages/Home/indexHooks.js
@@ -1,33 +1,45 @@
 import React, { useState } from "react";
 import Search from "../../components/Search";
 import Logo from "../../components/Logo";
-// import { getUser } from "../../services/api/users";
+import { getUser } from "../../services/api/users";
 
 import "./style.css";
 
 const Home = () => {
 
-    // prevState inicial vazio
-    const [valueInput, setValueInput] = useState("");
-    const [user, setUser] = useState({});
-    // const [error, setError] = useState("");
+  // prevState inicial vazio
+  const [valueInput, setValueInput] = useState("");
+  const [user, setUser] = useState({});
+  // const [error, setError] = useState("");
 
-    console.log(valueInput)
+  console.log(valueInput)
 
-    return (
-        <div className="container__home">
-          <div>
-            <Logo classGit="Github-Search" classSearch="text-style-1" />
-          </div>
-          <div>
-            <Search
-              classSearch="text-style-1"
-              typing={(e) => setValueInput(e.target.value)}
-            // click={this.searching}
-            />
-          </div>
-        </div>
-      );
+  function searching() {
+    if (valueInput !== "") {
+      getUser(valueInput)
+        .then(response => {
+          setUser(response)
+        }
+        )
+    };
+  }
+
+  console.log(user)
+
+  return (
+    <div className="container__home">
+      <div>
+        <Logo classGit="Github-Search" classSearch="text-style-1" />
+      </div>
+      <div>
+        <Search
+          classSearch="text-style-1"
+          typing={(e) => setValueInput(e.target.value)}
+          click={() => searching()}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home
